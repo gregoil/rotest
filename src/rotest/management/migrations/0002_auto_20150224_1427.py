2b3df42f77c7277369631c1b31266a41526bf90c
Osher De Paz <osherdepaz@gmail.com>
2018-03-09 16:06:28 +0200

    Revert the superuser creation in a migration

--- src/rotest/management/migrations/0002_auto_20150224_1427.py
+++ src/rotest/management/migrations/0002_auto_20150224_1427.py
@@ -4,6 +4,9 @@ from __future__ import unicode_literals
 from django.db import migrations
 from django.contrib.auth import models as auth_models
 
+ADMIN_USERNAME = "rotest"
+ADMIN_PASSWORD = "rotest"
+
 
 def create_users(apps, schema_editor):
     qa_group, _ = auth_models.Group.objects.get_or_create(name="QA")
@@ -11,6 +14,13 @@ def create_users(apps, schema_editor):
                                                           password="localhost",
                                                           email="l@l.com")
     qa_group.user_set.add(localhost)
+
+    try:
+        auth_models.User.objects.get(username=ADMIN_USERNAME)
+    except auth_models.User.DoesNotExist:
+        auth_models.User.objects.create_superuser(ADMIN_USERNAME,
+                                                  "rotest@rotest.com",
+                                                  ADMIN_PASSWORD)
 
 
 class Migration(migrations.Migration):
