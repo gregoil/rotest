63a2d212aaa3c4988912a2a8208abbbf5c1cfe4c
Osher De Paz <osherdepaz@gmail.com>
2018-05-09 19:29:58 +0300

    bumped version to 2.8.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 
 from setuptools import setup, find_packages
 
-__version__ = "2.8.3"
+__version__ = "2.8.4"
 
 result_handlers = [
     "db = rotest.core.result.handlers.db_handler:DBHandler",
