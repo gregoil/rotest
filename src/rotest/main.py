31aad732079d56c2c592d84410dc5395a18ad0e4
Osher De Paz <osherdepaz@gmail.com>
2018-05-10 13:11:26 +0300

    doc_and_enabled_module

--- src/rotest/main.py
+++ src/rotest/main.py
@@ -1,19 +1,40 @@
 #!/usr/bin/env python
+import os
 import sys
+from unittest import loader
 from itertools import chain
 
+from rotest.core import TestSuite, TestCase, TestFlow, runner
+from rotest.management.server.main import main as server_main
 
-def do_discover(start_dir):
-    from unittest import loader
-    from rotest.core import TestCase, TestFlow
+
+def do_discover(path):
+    """Find and return all Rotest test items discovered under a directory.
+
+    Filters the tests so only return subclasses of TestCase and TestFlow
+    that their __test__ static field doesn't exist or set to False.
+
+    Args:
+        path (str): directory to begin the recursive search from, or module
+            to run.
+
+    Returns:
+        list. all discovered tests.
+    """
+    path = os.path.abspath(path)
     tests_loader = loader.defaultTestLoader
-
-    def loadTestsFromTestCase(testCaseClass):
-        return testCaseClass
-
     tests_loader.suiteClass = list
-    tests_loader.loadTestsFromTestCase = loadTestsFromTestCase
-    discovered_tests = tests_loader.discover(start_dir, '*.py', start_dir)
+    tests_loader.loadTestsFromTestCase = lambda test: test
+
+    if os.path.isdir(path):
+        start_dir = path
+        pattern = '*.py'
+
+    else:
+        start_dir = os.path.dirname(path)
+        pattern = os.path.basename(path)
+
+    discovered_tests = tests_loader.discover(start_dir, pattern, None)
 
     all_tests = []
     for test_class in chain(*discovered_tests):
@@ -33,17 +54,15 @@ def main():
     # Rotest admin script:
     argv = sys.argv[1:]
     if argv[0] == "server":
-        from rotest.management.server.main import main as server_main
+        del sys.argv[1]
         server_main()
 
-    elif argv[0] == "discover":
+    elif argv[0] == "run":
         tests = do_discover(argv[1])
-        from rotest.core import TestSuite, runner
 
         class AlmightySuite(TestSuite):
             components = tests
 
-        del argv[1:3]
         runner.main(AlmightySuite)
 
     # Default option - pass control to django-admin:
