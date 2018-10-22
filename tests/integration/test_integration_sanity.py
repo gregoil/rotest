733ab627a5cfce8d11c045cb2f12cf0e3fa83729
rinslow <elkissdan@gmail.com>
2018-10-22 11:40:58 +0300

    Made integration sanity test cross-platform

--- tests/integration/test_integration_sanity.py
+++ tests/integration/test_integration_sanity.py
@@ -4,15 +4,19 @@ import sys
 import subprocess
 
 THIS_FILE = os.path.abspath(__file__)
+THIS_DIRECTORY = os.path.dirname(THIS_FILE)
+ROTEST_WORK_DIR = os.path.join(os.path.expanduser("~"), ".rotest")
 
 
 def test_sanity():
     python = sys.executable
-    playground = os.path.join(os.path.dirname(THIS_FILE), "playground.py")
+    playground = os.path.join(THIS_DIRECTORY, "playground.py")
     environment_variables = {
         "DJANGO_SETTINGS_MODULE":
-            "rotest.common.django_utils.settings"
+            "rotest.common.django_utils.settings",
+        "ROTEST_WORK_DIR": ROTEST_WORK_DIR
     }
+
     p = subprocess.Popen([python, playground], env=environment_variables,
                          stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
