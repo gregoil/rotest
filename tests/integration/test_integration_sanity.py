33259935eb465fd2f3d11185c7da3224cdc53aa0
rinslow <elkissdan@gmail.com>
2018-10-22 11:00:23 +0300

    Refactored sanity tests to use subprocess

--- tests/integration/test_integration_sanity.py
+++ tests/integration/test_integration_sanity.py
@@ -1,22 +1,25 @@
 import os
 import sys
 
-import delegator
-
+import subprocess
 
 THIS_FILE = os.path.abspath(__file__)
 
 
 def test_sanity():
+    python = sys.executable
     playground = os.path.join(os.path.dirname(THIS_FILE), "playground.py")
-    process = delegator.run("{python} {playground}"
-                            .format(python=sys.executable,
-                                    playground=playground),
-                            env={"DJANGO_SETTINGS_MODULE":
-                                 "rotest.common.django_utils.settings"})
+    environment_variables = {
+        "DJANGO_SETTINGS_MODULE":
+            "rotest.common.django_utils.settings"
+    }
+    p = subprocess.Popen([python, playground], env=environment_variables,
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    process.block()
-    assert "Test run has started" in process.err
-    assert 'ExampleBlock1 ran' in process.out
-    assert 'ExampleBlock2 ran' in process.out
-    assert process.ok
+    return_code = p.wait()
+    stderr = p.stderr.read()
+    stdout = p.stdout.read()
+    assert b"Test run has started" in stderr
+    assert b'ExampleBlock1 ran' in stdout
+    assert b'ExampleBlock2 ran' in stdout
+    assert return_code == 0
