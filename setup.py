40584112ee835ee7795d196023f45b59f75bc558
Osher De Paz <osherdepaz@gmail.com>
2018-10-29 10:46:22 +0200

    bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from __future__ import absolute_import
 from setuptools import setup, find_packages
 
-__version__ = "5.1.0"
+__version__ = "5.1.1"
 
 result_handlers = [
     "db = rotest.core.result.handlers.db_handler:DBHandler",
