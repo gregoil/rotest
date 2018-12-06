4b52634fe71d36d7dd4b5e00ed326f2579fa46b4
Dan Elkis <elkissdan@gmail.com>
2018-12-06 12:38:46 +0200

    fixed pylint version and added environments for Python 3.7 at Travis and AppVeyor

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup(
                 "xlrd",
                 "pathlib2",
                 "flake8",
-                "pylint"]
+                "pylint < 1.8.0"]
     },
     python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*",
     entry_points={
