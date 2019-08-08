274e13bbe2003d76275435e48f29d66fea61f7e0
UnDarkle <edrafolt@gmail.com>
2019-08-08 10:22:34 +0300

    fixed the ws client ut

--- tests/api/channel_tests.py
+++ tests/api/channel_tests.py
@@ -4,10 +4,6 @@ import json
 import mock
 from channels.test import ChannelTestCase, Client
 
-from rotest.common.config import DJANGO_MANAGER_PORT
-from rotest.management.client.client import AbstractClient
-from rotest.management.client.websocket_client import PingingWebsocket
-
 
 class ChannelTests(ChannelTestCase):
     """Tests for consumer behavior."""
@@ -22,15 +18,4 @@ class ChannelTests(ChannelTestCase):
         message_content = {'text': json.dumps({"token": self.TEST_TOKEN})}
         client.send_and_consume('websocket.receive', message_content)
         client.send_and_consume('websocket.disconnect', {})
-        mock_callback.assert_called_with(self.TEST_TOKEN)
-
-    @mock.patch('rotest.management.client.websocket_client.'
-                'PingingWebsocket.handle_disconnection')
-    def test_server_disconnection(self, mock_callback):
-        """Check that 'close_session' is called at disconnect."""
-        client = PingingWebsocket(ping_interval=1)
-        server_address = AbstractClient.WEBSOCKET_TARGET.format(
-                                                    host="localhost",
-                                                    port=DJANGO_MANAGER_PORT)
-        client.connect(server_address)
         mock_callback.assert_called_with(self.TEST_TOKEN)
