9dea84c84eced5330e57369fd2bde343f59007e9
UnDarkle <edrafolt@gmail.com>
2019-09-12 16:43:53 +0300

    Update urls.py

--- src/rotest/common/django_utils/urls.py
+++ src/rotest/common/django_utils/urls.py
@@ -3,6 +3,8 @@ from __future__ import absolute_import
 from django.contrib import admin
 from django.conf.urls import include, url
 
+app_name = "rotest"
+
 admin.autodiscover()
 urlpatterns = [
     url(r'^rotest/api/', include("rotest.api.urls")),
