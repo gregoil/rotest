5b4c0e752e719783c87d0244d7bec0638b45f781
Greg <gregoil@walla.co.il>
2020-01-09 17:05:28 +0200

    limited django 1.11

--- setup.py
+++ setup.py
@@ -65,7 +65,6 @@ setup(
             "flake8",
             "pylint<2.4",
             "waiting",
-            "zope.interface<4.7.0"
         ]
     },
     python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*",
